379d9daaec5084c916b915998fe76133e9c2beb5
Phoenix R <phoenixrabang@gmail.com>
2020-01-14 13:27:18 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,21 @@
 # Add your variables here
+first_number = 2
+second_number = 5
+expect(second_number).not equal(0)
+first_number = numberone
+second_number = numbertwo
+def sum
+  puts numberone + numbertwo
+end
+
+def difference
+  puts numberone - numbertwo
+end
+
+def product
+  puts numberone * numbertwo
+end
+
+def quotient
+  puts numberone / numbertwo
+end
